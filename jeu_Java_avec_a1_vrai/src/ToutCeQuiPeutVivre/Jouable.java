9edb5326c77fcffefd33dccc207096f4ae1596b1
swann <swann@172.29.131.61>
2015-10-10 17:58:25 +0200

    ajout des setteur de rectangle

---
+++ jeu_Java_avec_a1_vrai/src/ToutCeQuiPeutVivre/Jouable.java
@@ -0,0 +1,24 @@
+package ToutCeQuiPeutVivre;
+
+import LesActionsSpecials.Attaque;
+
+public class Jouable extends Vivant{
+
+	public Jouable(int hpMax, int manaMax, int attaque, int defense,
+			Attaque[] listeAttaque, int modAttaque, int modDefense,
+			int modVitesse, int attaqueEquipement, int defenseEquipement,
+			int vitesseEquipement, int manaMaxEquipement, int hpMaxEquipement,
+			String type, int vitesse, int vitesseX, int vitesseY,
+			int accelerationX, int accelerationY, int masse) {
+		super(hpMax, manaMax, attaque, defense, listeAttaque, modAttaque, modDefense,
+				modVitesse, attaqueEquipement, defenseEquipement, vitesseEquipement,
+				manaMaxEquipement, hpMaxEquipement, type, vitesse, vitesseX, vitesseY,
+				accelerationX, accelerationY, masse);
+		
+	}
+	
+	
+
+
+
+}
