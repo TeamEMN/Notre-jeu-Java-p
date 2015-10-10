97765b6059d622bf81cf6256b2b77293ade84cf5
keussidou43 <herrgeneralboombomm@gmail.com>
2015-10-10 12:40:57 +0200

    Provencal : Zone pour Alexis

---
+++ jeu_Java_avec_a1_vrai/src/Lieux/Zone.java
@@ -0,0 +1,12 @@
+package Lieux;
+
+import Solides.Rectangles;
+
+public class Zone extends Rectangles {
+	
+	public Zone(int longueur, int largeur, int xSG, int ySG){
+		super(longueur, largeur, xSG, ySG);
+	}
+	
+
+}
---
+++ jeu_Java_avec_a1_vrai/src/Solides/Obstacle.java
@@ -0,0 +1,18 @@
+package Solides;
+
+// Pour coder nos obstacles graces aux rectangles 
+
+public class Obstacle extends Rectangles {
+	
+	public Obstacle()
+
+	
+	// get liste de rectangles
+	// set liste de rectangle
+	// create obstacle : appel successif des rectangles pour donner la liste complete de l'obstacle
+	// get url
+	// set url
+	
+}
+
+// TO DO : Mur, Plateforme, Hitbox
