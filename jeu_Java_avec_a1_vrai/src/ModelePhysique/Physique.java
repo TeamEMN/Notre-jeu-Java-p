9a5faadcc6fdcca9c35cbed092b7979a25e9f50d
Alexis Lozano <alexis.pascal.lozano@gmail.com>
2015-10-10 11:50:47 +0200

    pour alexis

--- jeu_Java_avec_a1_vrai/src/ModelePhysique/Physique.java
+++ jeu_Java_avec_a1_vrai/src/ModelePhysique/Physique.java
@@ -16,18 +16,24 @@ public class Physique {
 	}
 	
 	public void Newton(Vivant vivant) {
-		int SommeForceVivantX = 0; // Initialisation de la somme des forces projetées en x
-		int SommeForceVivantY = 0; // Initialisation de la somme des forces projetées en y
+		int SommeForceVivantX = 0; //Alexis : Initialisation de la somme des forces projetées en x
+		int SommeForceVivantY = 0; //Alexis : Initialisation de la somme des forces projetées en y
 		for (int i = 0; i < this.getListeForces().size(); i++) {
-			if (this.getListeForces().get(i).getVivant() == vivant) { // on ne prend que les forces affectées à vivant
-				SommeForceVivantX += this.getListeForces().get(i).getIntensiteX(); // on somme les forces en x
-				SommeForceVivantY += this.getListeForces().get(i).getIntensiteY(); // on somme les forces en y
+			if (this.getListeForces().get(i).getVivant() == vivant) { //Alexis : on ne prend que les forces affectées à vivant
+				SommeForceVivantX += this.getListeForces().get(i).getIntensiteX(); //Alexis : on somme les forces en x
+				SommeForceVivantY += this.getListeForces().get(i).getIntensiteY(); //Alexis : on somme les forces en y
 			}
 		}
-		vivant.setAccelerationX(SommeForceVivantX/vivant.getMasse()); // on balance l'accélération en x dans vivant 
-		vivant.setAccelerationY(SommeForceVivantY/vivant.getMasse()); // on balance l'accélération en y dans vivant
+		vivant.setAccelerationX(SommeForceVivantX/vivant.getMasse()); //Alexis : on balance l'accélération en x dans vivant 
+		vivant.setAccelerationY(SommeForceVivantY/vivant.getMasse()); //Alexis : on balance l'accélération en y dans vivant
 	}
-	
+	 public void Integration(Vivant vivant) {
+		 int dt = 1; //Alexis : À voir si on la hardcode ici on si on la met en argument (ou dans une autre classe)
+		 vivant.setVitesseX(vivant.getVitesseX()+vivant.getAccelerationX()*dt);
+		 vivant.setVitesseY(vivant.getVitesseY()+vivant.getAccelerationY()*dt);
+		 vivant.set
+		 
+	 }
 	
 
 }
