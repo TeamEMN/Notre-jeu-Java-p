d3dabeac1142cf9b18e867a85f26097e28e2c6bd
Alexis Lozano <alexis.pascal.lozano@gmail.com>
2015-10-14 09:54:12 +0200

    Alexis : ajout de la variable mecanisme à la classe Ponctuelle

--- jeu_Java_avec_a1_vrai/src/Force/Ponctuelle.java
+++ jeu_Java_avec_a1_vrai/src/Force/Ponctuelle.java
@@ -1,11 +1,22 @@
 package Force;
 import ToutCeQuiPeutVivre.Vivant;
+import Mecanismes.Mecanisme;
 import ModelePhysique.Force;
 
 public class Ponctuelle extends Force {
+	private Mecanisme _mecanisme;
 	
-	public Ponctuelle(Vivant vivant, int intensiteX, int intensiteY) {
+	public Ponctuelle(Vivant vivant, int intensiteX, int intensiteY, Mecanisme mecanisme) {
 		super(vivant,intensiteX,intensiteY);
+		_mecanisme = mecanisme;
 		
+	}
+
+	public Mecanisme getMecanisme() {
+		return _mecanisme;
+	}
+
+	public void setMecanisme(Mecanisme _mecanisme) {
+		this._mecanisme = _mecanisme;
 	}
 }
